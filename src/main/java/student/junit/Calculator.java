5699abf9df0afe48db12caa9a054bfddae589918
Matthew Haworth <matthewh2008@googlemail.com>
2016-02-08 14:00:23 +0000

    initial commit

---
+++ src/main/java/student/junit/Calculator.java
@@ -0,0 +1,24 @@
+package student.junit;
+
+public class Calculator
+{
+	public double add(double number1, double number2)
+	{
+		return number1 + number2;
+	}
+	
+	public double subtract(double number1, double number2)
+	{
+		return number1 - number2;
+	}
+	
+	public double multiply(double number1, double number2)
+	{
+		return number1 * number2;
+	}
+	
+	public double divide(double number1, double number2)
+	{
+		return number1 / number2;
+	}
+}
